d3a3e3854acb40c482567a009cfa04234e7acdbc
Kenfin2017 <kenfin1996@gmail.com>
2019-01-24 12:53:44 +0200

    Initialised project

---
+++ Weather_station/Weather_Station/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 1/24/2019 12:40:46 PM
+ * Author : Minh
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
